87c3a8995aafb6905124b9a28aad2e3a8ea1f059
Cesar Bittencourt <vibiconstato@gmail.com>
2026-06-08 17:27:57 -0300

    Atualizar webhook URL do n8n

--- env.js
+++ env.js
@@ -3,5 +3,5 @@
 window.__ENV__ = {
   SUPABASE_URL:      "https://tjyxnusbkxkuwmbgkiyb.supabase.co",
   SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRqeXhudXNia3hrdXdtYmdraXliIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA3MzgxMDksImV4cCI6MjA5NjMxNDEwOX0.8hAhKS47qyVvF2JhrcG0E5yeRxMESX1jwbkNU5UV9TM",
-  N8N_WEBHOOK_URL:   "https://n8n-production-d677.up.railway.app/webhook-test/3ccc3a66-2f12-45cd-8dec-d7c8b94df758"
+  N8N_WEBHOOK_URL:   "https://n8n-production-1b10.up.railway.app/webhook-test/ee328497-8b2d-48d4-8046-a7602d70559e"
 };
